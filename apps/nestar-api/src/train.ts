f7abc052dc787689007cb13e486e593b8caf41a6
unknown <shokirovbekzod6090@gmail.com>
2025-03-07 19:42:51 +0900

    feat: create components related business models

--- apps/nestar-api/src/train.ts
+++ apps/nestar-api/src/train.ts
@@ -3,20 +3,20 @@
 // Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin va 5 soniyadan keyin ishini toxtatsin.
 // MASALAN: printNumbers()
 
-function printNumbers() {
-    let num = 1;
-    const interval = setInterval(() => {
-        console.log(num);
-        num++;
-        if (num > 5) {
-            num = 1;
-        }
-    }, 1000);
+// function printNumbers() {
+//     let num = 1;
+//     const interval = setInterval(() => {
+//         console.log(num);
+//         num++;
+//         if (num > 5) {
+//             num = 1;
+//         }
+//     }, 1000);
     
-    setTimeout(() => {
-        clearInterval(interval);
-        console.log("Process stopped");
-    }, 5000);
-}
+//     setTimeout(() => {
+//         clearInterval(interval);
+//         console.log("Process stopped");
+//     }, 5000);
+// }
 
-printNumbers();
+// printNumbers();
