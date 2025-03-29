a5b08615b9efc32643602c78686d3233c19e5c8f
unknown <shokirovbekzod6090@gmail.com>
2025-03-29 22:09:24 +0900

    feat: develop createComment, updateComment and getComments GraphQL APIs

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -4,11 +4,14 @@ import { MemberService } from '../member/member.service';
 import { PropertyService } from '../property/property.service';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
 import { BoardArticleService } from '../board-article/board-article.service';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
-import { Message } from '../../libs/enums/common.enum';
-import { CommentGroup } from '../../libs/enums/comment.enum';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
 import { Model, ObjectId } from 'mongoose';
 import { Comment, Comments } from '../../libs/dto/comment/comment';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { lookupMember } from '../../libs/config';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class CommentService {
@@ -53,5 +56,46 @@ export class CommentService {
 		}
 		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
 		return result;
+	}
+	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
+		const { _id } = input;
+		const result = await this.commentModel.findOneAndUpdate(
+			{
+				_id: __dirname,
+				memberId: memberId,
+				commentStatus: CommentStatus.ACTIVE,
+			},
+			input,
+			{
+				new: true,
+			},
+		);
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+		return result;
+	}
+
+	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+		const { commentRefId } = input.search;
+		const match: T = { commentRefId: commentRefId, CommentStatus: CommentStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createAt']: input?.direction ?? Direction.DESC };
+
+		const result: Comments[] = await this.commentModel.aggregate([
+			{ $match: match },
+			{ $sort: sort },
+			{
+				$facet: {
+					list: [
+						{ $skip: (input.page - 1) * input.limit },
+						{ $limit: input.limit },
+						//me Liked
+						lookupMember,
+						{ $unwind: '$memberData' },
+					],
+				},
+			},
+		]);
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
 	}
 }
